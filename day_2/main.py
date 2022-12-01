05c12782a5cc0689bc06beaee25314bcecbb42ef
Endya <endya@protonmail.com>
2022-12-01 18:16:46 +0100

    day 2 part 1

--- day_2/main.py
+++ day_2/main.py
@@ -1,2 +1,17 @@
+import numpy as np
+import pandas as pd
+
 if __name__ == '__main__':
-	pass
+	# part 1
+	trajectory = pd.read_csv('input', sep = ' ', names = ['direction', 'value'])
+	forward = trajectory.loc[lambda df: df['direction'] == 'forward', :]
+	up = trajectory.loc[lambda df: df['direction'] == 'up', :]
+	down = trajectory.loc[lambda df: df['direction'] == 'down', :]
+
+	total_forward = sum(forward['value'])
+	total_depth = sum(down['value']) - sum(up['value'])
+	print('The total forward move is : ', total_forward,
+		'and the total depth move is', total_depth,
+		'giving a product of', total_depth * total_forward)
+
+
