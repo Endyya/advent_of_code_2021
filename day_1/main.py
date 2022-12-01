ca614587e52fde3806f09bb5e0ddbf9179ebee64
Endya <endya@protonmail.com>
2022-12-01 17:34:53 +0100

    day 1 part 2 finished

--- day_1/main.py
+++ day_1/main.py
@@ -5,4 +5,15 @@ if __name__ == '__main__':
 	table = np.loadtxt('input', dtype = int)
 	diff = table[1:] - table[:-1]
 	print('There is', len(diff[diff > 0]), 'increased depth measured')
-	
+
+	# part 2
+	window_length = 3
+	cumul_table = np.zeros(len(table) - window_length + 1, dtype = int)
+	for i in range(window_length):
+		start = i
+		stop = len(table) + i - window_length
+		cumul_table += table[start:(stop + 1)]
+
+	diff = cumul_table[1:] - cumul_table[:-1]
+	print('There is', len(diff[diff > 0]),
+		f'increased depth measured (given a window of {window_length})')
