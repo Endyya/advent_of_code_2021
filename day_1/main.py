ef8f74f80c208f09bd08c7039ace2941570a69b6
Endya <endya@protonmail.com>
2022-12-01 17:06:31 +0100

    part 1 day 1 finished

--- day_1/main.py
+++ day_1/main.py
@@ -1,2 +1,8 @@
+import numpy as np
+
 if __name__ == '__main__':
-	pass
+	# part 1
+	table = np.loadtxt('input', dtype = int)
+	diff = table[1:] - table[:-1]
+	print('There is', len(diff[diff > 0]), 'increased depth measured')
+	
